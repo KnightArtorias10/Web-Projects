9c5c0d914f747cb76810aca63570008c32c8ac44
KnightArtorias10 <36730164+KnightArtorias10@users.noreply.github.com>
2018-07-18 09:21:15 +0200

    Registration

---
+++ Registration/src/Servlets/Login.java
@@ -0,0 +1,20 @@
+package Servlets;
+
+public class Login {
+
+    public Login() {}
+    //* Property numberOfVisitors.
+    private int numberOfVisitors;
+    //* Getter für Property numberOfVisitors.
+    public int getNumberOfVisitors() {
+        return numberOfVisitors;
+    }
+    //* Setter für Property numberOfVisitors.
+    public void setNumberOfVisitors(int numberOfVisitors) {
+        this.numberOfVisitors = numberOfVisitors;
+    }
+    //* Zählt einen Besucher.
+    public void countVisitor() {
+        numberOfVisitors++;
+    }
+}
